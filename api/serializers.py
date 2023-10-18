af55e35cc7c48c4c79b16bd9f781840d850ff98b
Vaza <raisvaza@gmail.com>
2023-10-18 23:24:33 +0700

    feat: add Serializer for Pos, Reading, and Predict Model

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
-from base.models import Item
+from base.models import Item, Pos, Reading, Predict
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
+        fields = '__all__'
+
+class PosSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pos
+        fields = '__all__'
+
+class ReadingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reading
+        fields = '__all__'
+
+class PredictSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Predict
         fields = '__all__'

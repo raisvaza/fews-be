d4cf4977f3d76d2b4fe1368e8526b3cc5c91a2b7
Vaza <raisvaza@gmail.com>
2023-10-18 23:25:19 +0700

    feat: add view to get predict and pos

--- api/views.py
+++ api/views.py
@@ -1,10 +1,12 @@
+from django.db.models import Max, F
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from base.models import Item
-from .serializers import ItemSerializer
+from base.models import Item, Pos, Reading, Predict
+from .serializers import ItemSerializer, PosSerializer, ReadingSerializer, PredictSerializer
+
 
 @api_view(['GET'])
-def getData(request):
+def getItem(request):
     items = Item.objects.all()
     serializer = ItemSerializer(items, many=True)
     return Response(serializer.data)
@@ -14,4 +16,49 @@ def addItem(request):
     serializer = ItemSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_pos(request):
+    pos = Pos.objects.all()
+    serializer = PosSerializer(pos, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_reading(request):
+    reading = Reading.objects.all()
+    serializer = ReadingSerializer(reading, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_predict(request):
+    predict = Predict.objects.all()
+    serializer = PredictSerializer(predict, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_latest_predict_for_every_pos(request):
+
+    pos_all = Pos.objects.filter(tipe = "DugaAir")
+
+    latest_predict = []
+
+    for pos in pos_all:
+        predict = Predict.objects.filter(pos_id = pos).order_by('predict_for').first()
+        latest_predict.append(predict)
+
+    print(latest_predict)
+
+
+
+    # predict = Predict.objects.all()
+
+    # most_recent_predicts = Predict.objects.values('pos_id_id').annotate(
+    #     max_predict_at=Max('predict_for')
+    # ).filter(
+    #     predict_for=F('max_predict_at')
+    # )
+
+
+    serializer = PredictSerializer(latest_predict, many=True)
     return Response(serializer.data)
